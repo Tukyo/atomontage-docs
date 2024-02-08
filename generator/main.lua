9082063dfcc231d7c426e298f5700b240b615e96
Max Kriester <max.kriester@gmail.com>
2024-02-08 19:12:43 +0530

    Fix path

--- generator/main.lua
+++ generator/main.lua
@@ -16,11 +16,11 @@ end
 
 --change on other computer, api files are copied here
 local apiFrom = "C:\\Users\\maxkr\\Documents\\Code\\Atomontage\\atomontage-docs\\generator\\emmyApi"
-local apiTo = "D:\\atomontage\\Resources\\Sdk\\LuaApi\\Atomontage\\library"
+local apiTo = "D:\\atomontage\\Resources\\Studio\\LuaApi\\Atomontage\\library"
 copyFile(apiFrom, apiTo)
 
 local systemFrom = "D:\\atomontage\\Build\\win64_vs2022\\Studio\\RelWithDebInfo\\Data\\Studio\\Script\\ae"
-local systemTo = "D:\\atomontage\\Resources\\Sdk\\LuaApi\\Atomontage\\library\\systemScripts\\ae"
+local systemTo = "D:\\atomontage\\Resources\\Studio\\LuaApi\\Atomontage\\library\\systemScripts\\ae"
 copyFile(systemFrom, systemTo)
 
 print("done")
